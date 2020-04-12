f0731241688cbc8e87886dfb9c486587f5ceddd3
Satvik <ssatvikgupta70@gmail.com>
2020-04-12 17:24:23 +0530

    add avatars sockets and redis

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+const Like = require('../models/like');
 
 module.exports.create = async function(req, res){
 
@@ -15,11 +19,37 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
+            
+            comment = await comment.populate('user', 'name email').execPopulate();
+            // commentsMailer.newComment(comment);
+
+            let job = queue.create('emails', comment).save(function(err){
+                if (err){
+                    console.log('Error in sending to the queue', err);
+                    return;
+                }
+                console.log('job enqueued', job.id);
+
+            })
+
+            if (req.xhr){
+                
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
 
             res.redirect('/');
         }
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     
@@ -39,12 +69,30 @@ module.exports.destroy = async function(req, res){
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
 
+            // CHANGE :: destroy the associated likes for this comment
+            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
+
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
         }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     
--- models/post.js
+++ models/post.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+const IMAGE_PATH = path.join('/uploads/posts/images');
 
 const postSchema = new mongoose.Schema({
     content: {
@@ -11,16 +14,40 @@ const postSchema = new mongoose.Schema({
         ref: 'User'
 
     },
+
+    avatar: {
+        type: String
+    },
     // include the array of ids of all comments in this post schema itself
     comments: [
         {
             type:  mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
         }
+    ],
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like'
+        }
     ]
 },{
     timestamps: true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', IMAGE_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+
+// static
+postSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+postSchema.statics.avatarPath = IMAGE_PATH;
 
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
